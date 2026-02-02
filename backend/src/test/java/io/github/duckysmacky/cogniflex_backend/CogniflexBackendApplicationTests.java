4e480ff3467a90ac412f4bb18928345805e3962c
nikdor <kriperoka@gmail.com>
2026-02-02 23:05:43 +0300

    Setup Spring Boot project for backend

---
+++ backend/src/test/java/io/github/duckysmacky/cogniflex_backend/CogniflexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.duckysmacky.cogniflex_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogniflexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
