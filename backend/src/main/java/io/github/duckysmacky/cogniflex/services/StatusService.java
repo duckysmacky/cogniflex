cc4ecb9c7594c482a71d7bc86e30246751dddbcc
DOOMB <goworuha.egor@yandex.ru>
2026-04-10 12:58:19 +0300

    model availability placehouldier

--- backend/src/main/java/io/github/duckysmacky/cogniflex/services/StatusService.java
+++ backend/src/main/java/io/github/duckysmacky/cogniflex/services/StatusService.java
@@ -1,10 +1,10 @@
 package io.github.duckysmacky.cogniflex.services;
 
-import io.github.duckysmacky.cogniflex_backend.Dtos.StatusResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.availability.ApplicationAvailability;
 import org.springframework.stereotype.Service;
+
+import io.github.duckysmacky.cogniflex.dto.StatusResponse;
 
 import java.time.Instant;
 
@@ -14,12 +14,19 @@ public class StatusService {
     @Autowired
     private ApplicationAvailability availability;
 
+    private ModelAvailabilityService model_availability;
+
+    public StatusService()
+    {
+        this.model_availability = new ModelAvailabilityService();
+    }
+
     public StatusResponse getStatus() {
 
         return new StatusResponse(
                 availability.getLivenessState().toString() == "CORRECT" ? "UP" : "DOWN",
                 availability.getReadinessState().toString() == "ACCEPTING_TRAFFIC" ? "UP" : "DOWN",
-                "NOT_CONNECTED_YET",
+                model_availability.getStatus(),
                 Instant.now().toString()
         );
     }
