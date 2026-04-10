b4dee35c4bf57da21539cf1a9cb5af63defd481e
DuckySmacky <kriperoka@gmail.com>
2026-04-10 21:16:20 +0300

    Add Spring Security config

---
+++ backend/src/main/java/io/github/duckysmacky/cogniflex/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package io.github.duckysmacky.cogniflex.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+            .cors(Customizer.withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authorizeHttpRequests(auth -> {
+                auth.requestMatchers(
+                    "/swagger-ui/**",
+                    "/swagger-ui.html"
+                ).permitAll();
+                auth.anyRequest().permitAll();
+            })
+            .headers(headers ->
+                headers
+                    .frameOptions(HeadersConfigurer.FrameOptionsConfig::deny)
+                    .contentSecurityPolicy(csp -> csp.policyDirectives("default-src 'self'"))
+            )
+            .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // TODO: add proper CORS config here
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
