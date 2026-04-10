809af7d8cf36a794c64a647e476b00c9325f492d
DOOMB <goworuha.egor@yandex.ru>
2026-04-10 12:30:47 +0300

    health check done?

--- backend/src/main/java/io/github/duckysmacky/cogniflex/services/StatusService.java
+++ backend/src/main/java/io/github/duckysmacky/cogniflex/services/StatusService.java
@@ -1,6 +1,9 @@
 package io.github.duckysmacky.cogniflex.services;
 
-import io.github.duckysmacky.cogniflex.dto.StatusResponse;
+import io.github.duckysmacky.cogniflex_backend.Dtos.StatusResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.availability.ApplicationAvailability;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -8,10 +11,14 @@ import java.time.Instant;
 @Service
 public class StatusService {
 
+    @Autowired
+    private ApplicationAvailability availability;
+
     public StatusResponse getStatus() {
+
         return new StatusResponse(
-                "UP",
-                "UP",
+                availability.getLivenessState().toString() == "CORRECT" ? "UP" : "DOWN",
+                availability.getReadinessState().toString() == "ACCEPTING_TRAFFIC" ? "UP" : "DOWN",
                 "NOT_CONNECTED_YET",
                 Instant.now().toString()
         );
