5bb62d536176b42615a04115e7375ea44c3537c7
DuckySmacky <kriperoka@gmail.com>
2026-04-10 21:36:08 +0300

    Enable Swagger only for dev profile

--- backend/src/main/java/io/github/duckysmacky/cogniflex/config/SecurityConfig.java
+++ backend/src/main/java/io/github/duckysmacky/cogniflex/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
             )
             .authorizeHttpRequests(auth -> {
                 auth.requestMatchers(
+                    "/v3/api-docs/**",
                     "/swagger-ui/**",
                     "/swagger-ui.html"
                 ).permitAll();
