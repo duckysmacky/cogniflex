bf4ff94477c663e6f91219837a1a2b716f2d5125
DOOMB <goworuha.egor@yandex.ru>
2026-04-19 15:38:15 +0300

    Metrics and status logic add

--- backend/src/main/java/io/github/duckysmacky/cogniflex/repositories/HistoryRepository.java
+++ backend/src/main/java/io/github/duckysmacky/cogniflex/repositories/HistoryRepository.java
@@ -2,8 +2,10 @@ package io.github.duckysmacky.cogniflex.repositories;
 
 import io.github.duckysmacky.cogniflex.entities.HistoryRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface HistoryRepository extends JpaRepository<HistoryRecord, UUID> {
 }
