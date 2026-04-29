7cb0d5cef8f3f2c9ed301b85392c816c652873c8
DOOMB <goworuha.egor@yandex.ru>
2026-04-30 01:34:27 +0300

    Add prod profile annotation

--- backend/src/main/java/io/github/duckysmacky/cogniflex/config/RateLimitConfig.java
+++ backend/src/main/java/io/github/duckysmacky/cogniflex/config/RateLimitConfig.java
@@ -2,12 +2,14 @@ package io.github.duckysmacky.cogniflex.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.github.duckysmacky.cogniflex.services.RateLimitInterceptor;
 
 @Configuration
+@Profile("prod")
 public class RateLimitConfig implements WebMvcConfigurer {
     
     @Autowired
