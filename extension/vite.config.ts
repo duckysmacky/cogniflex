903de7919766487f2693a965199f0d456127c63f
Namik Guseinov <gnamik007@gmail.com>
2026-05-08 19:51:05 +0300

    Added zipper

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -3,11 +3,18 @@ import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import zip from 'vite-plugin-zip-pack';
 import manifest from './manifest.config';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), crx({ manifest }), tailwindcss()],
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    crx({ manifest }),
+    zip({ outDir: 'release', outFileName: 'cogniflex-release.zip' }),
+    tailwindcss(),
+  ],
   resolve: {
     tsconfigPaths: true,
   },
